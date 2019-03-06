fc0f2a7794c5b205efb69fb0b6dad4c6d0bcab93
fy <fengyi@wondershare.cn>
2019-03-06 17:05:36 +0800

    工程名修改

--- Pods/Target Support Files/Pods-MKQIZTravel/Pods-MKQIZTravel-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MKQIZTravel : NSObject
-@end
-@implementation PodsDummy_Pods_MKQIZTravel
-@end
---
+++ Pods/Target Support Files/Pods-RfoxTravel/Pods-RfoxTravel-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RfoxTravel : NSObject
+@end
+@implementation PodsDummy_Pods_RfoxTravel
+@end
